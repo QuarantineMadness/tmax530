2bf49345a9564778c73fc2299ec36b94dabc6faf
LemaMoulaga <60754813+LemaMoulaga@users.noreply.github.com>
2020-04-22 13:19:07 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let startPlay
+let endgame= "true"
+let gamescore
+let timegame = 15
+
+
+const startGame= document.querySelector("startGame")
+const score = document.querySelector("score")
+const timer = document.querySelector("time")
+const clicks = document.querySelector("clickRate")
+const clickZone=document.querySelector("clickhere")
